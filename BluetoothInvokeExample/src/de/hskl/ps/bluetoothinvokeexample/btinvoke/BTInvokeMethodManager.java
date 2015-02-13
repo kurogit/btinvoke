e2ed54042cbbda8f5b0872935887b3ada422cad6
Patrick Schwartz <pasc0024@stud.hs-kl.de>
2015-02-13 20:20:46 +0100

    [#12] Documented BTInvokeMethodManager

--- BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/BTInvokeMethodManager.java
+++ BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/BTInvokeMethodManager.java
@@ -5,64 +5,84 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import de.hskl.ps.bluetoothinvokeexample.btinvoke.exceptions.MethodCallException;
 
+/**
+ * Handles the with BTInvoke callable methods.
+ * <p>
+ * Before a method can be invoked with BTInvoke a Interface containing the Method and a Class
+ * implementing the Interface have to be registered.
+ * <p>
+ * <b>Limitations:</b> A method that should be callable can not be overloaded in the interface or
+ * implementing class. Will also not work correctly if multiple interfaces with methods with same
+ * names were registered.
+ * <p>
+ * This class is a Singleton.
+ * 
+ * @author Patrick Schwartz
+ * @date 2015
+ */
 public class BTInvokeMethodManager {
-    
+
+    /** Map containing the callable Interfaces and their implementation */
+    private Map<Class<?>, Object> map = null;
+
+    /** The Singleton instance */
     private static BTInvokeMethodManager instance_ = null;
-    
+
+    /**
+     * Get the instance of this class.
+     * 
+     * @return The instance of this class.
+     */
     public static BTInvokeMethodManager getInstance() {
         if(instance_ == null)
             instance_ = new BTInvokeMethodManager();
-        
+
         return instance_;
     }
-    
-    private Map<Class<?>, Object> map = null;
-    
+
+    /**
+     * Register a interface and a instance of a Class implementing it.
+     * 
+     * @param interfaceClass
+     *            The interface.
+     * @param impl
+     *            An instance of a Class implementing the interface.
+     */
     public void registerInterfaceAndImplementation(Class<?> interfaceClass, Object impl) {
-        if(!interfaceClass.isInterface())
+        if(!interfaceClass.isInterface() || !interfaceClass.isInstance(impl))
             return;
-        
+
         map.put(interfaceClass, impl);
     }
-    
-    public Object callMethodFromJSON(String jsonString) throws MethodCallException {
-        try {
-            JSONObject j = new JSONObject(jsonString);
-            
-            String methodName = j.getString("method");
-            JSONArray jsonParams = j.getJSONArray("params");
-            
-            Object[] params = new Object[jsonParams.length()];
-            Class<?>[] paramTypes = new Class<?>[jsonParams.length()];
-            
-            for(int i = 0; i < jsonParams.length(); ++i) {
-                JSONObject param = jsonParams.getJSONObject(i);
-                
-                paramTypes[i] = Class.forName(param.getString("type"));
-                params[i] = param.get("value");
-            }
-            
-            return callMethod(methodName, params);
-            
-        } catch(JSONException e) {
-            throw new MethodCallException("Could not convert Json", e);
-        } catch(ClassNotFoundException e) {
-            throw new MethodCallException("Could not convert Json", e);
-        }
-    }
-    
+
+    /**
+     * Call a method in on of the registered Interfaces.
+     * <p>
+     * This Method will search the registered Interfaces for a method with the specified name and
+     * call the implementation of it. It will call the first method it finds with the name
+     * regardless if it has the correct Parameters or not.
+     * 
+     * @param methodName
+     *            Name of the method to call.
+     * @param params
+     *            The Parameters for the method.
+     * @return Result of the method.
+     * @throws MethodCallException
+     *             If the Method was not callable. Reasons this can happen include things such as:<br>
+     *             <ul>
+     *             <li> Method was not found. <li> Incorrect number of Parameters. <li> Incorrect
+     *             type of Parameters.
+     *             </ul>
+     */
     public Object callMethod(String methodName, Object[] params) throws MethodCallException {
         Set<Class<?>> keys = map.keySet();
-        
+
         Method method = null;
         Class<?> in = null;
-        
+
+        // Search the method in the interfaces.
         for(Class<?> i : keys) {
             Method[] methods = i.getDeclaredMethods();
             for(Method m : methods) {
@@ -75,17 +95,20 @@ public class BTInvokeMethodManager {
             if(in != null && method != null)
                 break;
         }
-                   
+
+        // Get the implementation.
         Object impl = map.get(in);
-        
+
         try {
+            // Call the Method.
             Object r = method.invoke(impl, params);
             return r;
         } catch(Exception e) {
             throw new MethodCallException("Unable to call the Method", e);
-        } 
+        }
     }
-    
+
+    /** Constructor taking no Parameters */
     private BTInvokeMethodManager() {
         map = new HashMap<Class<?>, Object>();
     }
