cb996904346b00ed738f8ed4fa63b9cca8566f41
Patrick Schwartz <pasc0024@stud.hs-kl.de>
2015-02-13 19:32:08 +0100

    [#12] Documented the Exceptions

--- BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/exceptions/BTConnectionException.java
+++ BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/exceptions/BTConnectionException.java
@@ -1,10 +1,18 @@
 package de.hskl.ps.bluetoothinvokeexample.btinvoke.exceptions;
 
+/**
+ * Exception for errors during the connection process.
+ * 
+ * @author Patrick Schwartz
+ */
 public class BTConnectionException extends Exception {
+
+    private static final long serialVersionUID = -2513028870604807218L;
+
     public BTConnectionException(String what) {
         super(what);
     }
-    
+
     public BTConnectionException(String what, Throwable t) {
         super(what, t);
     }
--- BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/exceptions/MethodCallException.java
+++ BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/exceptions/MethodCallException.java
@@ -1,17 +1,16 @@
 package de.hskl.ps.bluetoothinvokeexample.btinvoke.exceptions;
 
+/**
+ * Exception for errors while calling a method on the client device.
+ * 
+ * @author Patrick Schwartz
+ * @date 2015
+ *
+ */
 public class MethodCallException extends Exception {
 
     private static final long serialVersionUID = 5700037900409905815L;
 
-    public MethodCallException(String what) {
-        super(what);
-    }
-
-    public MethodCallException(Throwable t) {
-        super(t);
-    }
-    
     public MethodCallException(String what, Throwable t) {
         super(what, t);
     }
