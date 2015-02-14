8f67d2cf7f7c1b3b5890bb9be8f3086526c0fb4d
Patrick Schwartz <pasc0024@stud.hs-kl.de>
2015-02-14 14:12:55 +0100

    [#12] Document BTConnectionMessages.

--- BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/bluetooth/BTConnectionMessages.java
+++ BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/bluetooth/BTConnectionMessages.java
@@ -2,36 +2,65 @@ package de.hskl.ps.bluetoothinvokeexample.btinvoke.bluetooth;
 
 import android.content.Intent;
 
+/**
+ * Constants for various connection status messages. Used in broadcasts.
+ * <p>
+ * Errors are defined in this class. Messages for direct status are the
+ * {@link ConnectionStatus#ordinal()} values of the {@link ConnectionStatus} enum.
+ * 
+ * @author Patrick Schwartz
+ * @date 2015
+ */
 public final class BTConnectionMessages {
 
+    /**
+     * Action for a connection status message.
+     * <p>
+     * An intent with this action will always contain the extra
+     * {@link BTConnectionMessages#EXTRA_TYPE}.
+     */
     public static final String CONNECTION_STATUS_MESSAGE = "CONNECTION_STATUS_MESSAGE";
 
+    /** Extra for the status message type */
     public static final String EXTRA_TYPE = "EXTRA_TYPE";
+    /** Extra for the device name on connection */
     public static final String EXTRA_DEVICE = "EXTRA_DEVICE";
-
-    private static final int DEFAULT_TYPE = -255;
 
     private BTConnectionMessages() {
     }
 
+    /** Errors */
     public final static class Errors {
-        public final static int NOT_CONNECTED = -1;
+        /** Bluetooth is not connected */
+        public static final int NOT_CONNECTED = -1;
+        /** Bluetooth is disabled */
         public static final int BLUETOOTH_DISABLED = -2;
+        /** A connection is already being established */
         public static final int ALREADY_CONNECTING = -3;
+        /** A connection is already present */
         public static final int ALREADY_CONNECTED = -4;
+        /** The connecting failed */
         public static final int CONNECTING_FAILED = -5;
+        /** There are no bonded devices */
         public static final int NO_BONDED_DEVICES = -6;
 
         private Errors() {
         }
     }
 
+    /**
+     * Turns an intent with the {@link BTConnectionMessages#CONNECTION_STATUS_MESSAGE} action to a
+     * String containing a readable representation of the status message.
+     * 
+     * @param i The intent.
+     * @return A readable representation of the status message.
+     */
     public static final String turnIntentToHumanReadableString(Intent i) {
         // Only handle CONNECTION_STATUS_MESSAGE intents.
         if(!i.getAction().equalsIgnoreCase(CONNECTION_STATUS_MESSAGE))
             return "";
 
-        int type = i.getIntExtra(EXTRA_TYPE, DEFAULT_TYPE);
+        int type = i.getIntExtra(EXTRA_TYPE, -255);
 
         String m = "Bluetooth: ";
         // Switch does not work here
