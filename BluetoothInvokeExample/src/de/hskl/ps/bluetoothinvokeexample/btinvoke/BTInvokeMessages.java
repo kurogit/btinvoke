a55465cebbd7677bd6b4292da311e9946596d8f6
Patrick Schwartz <pasc0024@stud.hs-kl.de>
2015-02-14 14:53:49 +0100

    [#12] Document BTInvokeMessages.

--- BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/BTInvokeMessages.java
+++ BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/BTInvokeMessages.java
@@ -1,45 +1,88 @@
 package de.hskl.ps.bluetoothinvokeexample.btinvoke;
 
 import android.content.Intent;
+import de.hskl.ps.bluetoothinvokeexample.btinvoke.services.BTInvokeClientService;
+import de.hskl.ps.bluetoothinvokeexample.btinvoke.services.BTInvokeServerService;
 
-
+/**
+ * Constants for various BTInvoke status messages. Used in broadcasts.
+ * 
+ * @author Patrick Schwartz
+ * @date 2015
+ */
 public final class BTInvokeMessages {
+    /** 
+     * Intent action for a remote invocation request.
+     * <p>
+     * Intent contains the Extra {@link BTInvokeMessages.Extras#JSONSTRING}. 
+     */
     public static final String REMOTE_INVOCATION = "REMOTE_INVOCATION";
+    /** 
+     * Intent action for a remote invocation result.
+     * <p>
+     * Intent contains the Extra {@link BTInvokeMessages.Extras#JSONSTRING}. 
+     */
     public static final String REMOTE_INVOCATION_RESULT = "REMOTE_INVOCATION_RESULT";
     
+    /**
+     * Intent action for a BTInvoke status message. 
+     * <p>
+     * Used by {@link BTInvokeServerService} and {@link BTInvokeClientService}.
+     * Will always contain the Extra {@link BTInvokeMessages.Extras#STATUS_TYPE}.
+     */
     public static final String ACTION_STATUS_MESSAGE = "ACTION_STATUS_MESSAGE";
     
+    /** Status messages */
     public static final class Status {
+        /** A new request was received on the server side. */
         public static final int NEW_INVOCATION_REQUEST = 1;
+        /** A result was recieved from the client side. */
         public static final int RECIEVED_RESULT = 2;
+        /** The client side is currently handling a request. */
         public static final int HANDLING_REQUEST = 3;
+        /** The client side finished calling the method */
         public static final int METHOD_CALL_DONE = 4;
         
         private Status(){}
     }
     
+    /** Error messages */
     public static final class Errors {
+        /** Failed to send the String over Bluetooth */
         public static final int SENDING_STRING_FAILED = -1;
+        /** Failed to read String over Bluetooth */
         public static final int READING_STRING_FAILED = -2;
+        /** Failed to call the Method on the client side */
         public static final int CALLING_METHOD_FAILED = -3;
         
         private Errors(){}
     }
     
+    /** Keys for Extras in the messages */
     public static final class Extras {
+        /** The type of status message */
         public static final String STATUS_TYPE = "TYPE";
-        public static final String RESULT = "RESULT";
+        /** The JSON String */
         public static final String JSONSTRING = "JSONSTRING";
         
         private Extras(){}
     }
     
+    /** Special values for the result */
     public static final class Result {
+        /** There was an error on the client side */
         public static final String ERROR_RESULT = "error";
 
         private Result(){}
     }
     
+    /**
+     * Turns an intent with the {@link BTInvokeMessages#ACTION_STATUS_MESSAGE} action to a
+     * String containing a readable representation of the status message.
+     * 
+     * @param i The intent.
+     * @return A readable representation of the status message.
+     */
     public static final String turnIntentToHumanReadableString(Intent i) {
         if(!i.getAction().equalsIgnoreCase(ACTION_STATUS_MESSAGE)) {
             return "";
