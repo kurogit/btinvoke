6980077e70fcc8c1992d4d55c00ccda6ad709c6a
Patrick Schwartz <pasc0024@stud.hs-kl.de>
2015-02-15 14:17:28 +0100

    Simplification of InvocationHandler

--- BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/BTInvocationHandler.java
+++ BluetoothInvokeExample/src/de/hskl/ps/bluetoothinvokeexample/btinvoke/BTInvocationHandler.java
@@ -7,7 +7,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -16,7 +15,6 @@ import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 import de.hskl.ps.bluetoothinvokeexample.btinvoke.BTInvokeMessages.Result;
 import de.hskl.ps.bluetoothinvokeexample.btinvoke.exceptions.BTInvocationException;
-import de.hskl.ps.bluetoothinvokeexample.btinvoke.helper.RemoteInvocationRequest;
 import de.hskl.ps.bluetoothinvokeexample.btinvoke.helper.RemoteInvocationResult;
 import de.hskl.ps.bluetoothinvokeexample.util.BetterLog;
 
@@ -40,8 +38,9 @@ import de.hskl.ps.bluetoothinvokeexample.util.BetterLog;
  * <strong>Warning:</strong> This works by blocking the call until the
  * {@link BTInvokeMessages#REMOTE_INVOCATION_RESULT} arrives using a {@link BroadcastReceiver}. A
  * {@link BroadcastReceiver} is always called on the UI thread. If a Method on the created Proxy
- * object is also called from the UI thread, the broadcast containing the result can never be received. Therefore, methods
- * on an Proxy created with this {@code InvocationHandler} should never be called on the UI thread!
+ * object is also called from the UI thread, the broadcast containing the result can never be
+ * received. Therefore, methods on an Proxy created with this {@code InvocationHandler} should never
+ * be called on the UI thread!
  * 
  * @author Patrick Schwartz
  * @date 2015
@@ -49,15 +48,12 @@ import de.hskl.ps.bluetoothinvokeexample.util.BetterLog;
 public class BTInvocationHandler implements InvocationHandler {
 
     private static final String TAG = BTInvocationHandler.class.getSimpleName();
-    
-    /** Counter for IDs */
-    private static long currentID = 0;
-    
-    /** ID of the request this InvocationHandler will send */
-    private long id_ = -1;
+
+    private Context context_ = null;
+
     /** The result of the Method call */
     private Object result_ = null;
-    
+
     /** A latch for blocking the Method call until the result arrives */
     private CountDownLatch latch_ = null;
 
@@ -76,7 +72,7 @@ public class BTInvocationHandler implements InvocationHandler {
                     RemoteInvocationResult r = RemoteInvocationResult.fromJSONString(recievedString);
 
                     result_ = r.result();
-                    
+
                     // Release the block
                     latch_.countDown();
 
@@ -87,35 +83,28 @@ public class BTInvocationHandler implements InvocationHandler {
             }
         }
     };
-    
+
     /**
      * Creates a {@code BTInvocationHandler} Object. The context is needed for sending broadcasts.
      */
     public BTInvocationHandler(Context c) {
 
-        id_ = currentID++;
-        
+        context_ = c;
+
         latch_ = new CountDownLatch(1);
 
         broadcast_ = LocalBroadcastManager.getInstance(c);
         broadcast_.registerReceiver(broadCastReciever_, new IntentFilter(BTInvokeMessages.REMOTE_INVOCATION_RESULT));
     }
-    
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] params) throws Throwable {
-        // Turn method to json
-        JSONObject j = RemoteInvocationRequest.toJsonObject(id_, method.getName(), params);
 
-        BetterLog.d(TAG, "Created Json String: %s", j.toString());
-        
-        // send request to service
-        Intent intent = new Intent(BTInvokeMessages.REMOTE_INVOCATION);
-        intent.putExtra(BTInvokeMessages.Extras.JSONSTRING, j.toString());
-        broadcast_.sendBroadcast(intent);
+        BTInvoke.remoteExecute(context_, method.getName(), params);
 
         // Wait for result. Timeout after 10 seconds.
         boolean latchResult = latch_.await(10, TimeUnit.SECONDS);
-        
+
         // Prevent BroadcastReceiver from leaking
         broadcast_.unregisterReceiver(broadCastReciever_);
 
